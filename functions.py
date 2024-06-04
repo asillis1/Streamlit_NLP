23c8617201429a6941f6c69940882c2d76da3fdc
asillis1 <51338302+asillis1@users.noreply.github.com>
2024-06-04 10:28:07 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -10,6 +10,16 @@ from itertools import chain
 import os
 nltk.download('vader_lexicon')
 
+import spacy
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
+
 # File for defining functions used to run the app
 
 ################## VALIDATION FUNCTIONS
